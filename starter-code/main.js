f80e404d1867749076302f73d7c61add994e8f10
Dan Ha <danha@Dans-MacBook-Pro.local>
2017-01-12 13:28:39 -0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woohoo!")
+
+// Assignment 8.7: Create four variables that simulates the cards in the game
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
