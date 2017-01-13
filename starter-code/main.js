a5b85a477f8c9c527da30df8891417d68d78edd5
Dan Ha <danha@Dans-MacBook-Pro.local>
2017-01-12 17:19:00 -0800

    Unit 9 assignment - if statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,3 +6,28 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+// if (cardOne === cardTwo){
+// console.log('You found a match!')
+// }
+// else if (cardThree === cardFour){
+//   console.log('You found a match!')
+// }
+// else if (cardOne === cardThree){
+//   console.log('Sorry, try again.')
+// }
+// else if (cardOne === cardFour){
+//   console.log('Sorry, try again.')
+// }
+// else if (cardTwo === cardThree){
+//   console.log('Sorry, try again.')
+// }
+// else if (cardTwo === cardFour){
+//   console.log('Sorry, try again.')
+// }
+
+if (cardOne === cardTwo) {
+	alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
