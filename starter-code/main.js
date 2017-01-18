c0815acb6277f3aa138f6d796f07ae15fb596319
Dan Ha <danha@Dans-MacBook-Pro.local>
2017-01-17 16:03:36 -0800

    assignment Unit 11

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,31 +1,42 @@
-// var cardOne = 'queen';
-// var cardTwo = 'queen';
-// var cardThree = 'king';
-// var cardFour = 'king';
 
-// if (cardOne === cardTwo) {
-// 	alert('You found a match!');
-// } else {
-//   alert('Sorry, try again.');
-// }
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-// find the board and set it to a variable
-// so that we can add cards to it
+
 var board = document.getElementById('game-board');
 
-// function that will create your board
 function createBoard() {
-  // loop through your cards array to create card elements for your board
   for (var i=0; i<cards.length; i++) {
-
-    // create a div element which will be used as a card
     var cardElement = document.createElement('div');
-
-    // add a class to the card element which will help link styling
     cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
 
-    // append the card to the board
+    board.appendChild(cardElement);
     board.appendChild(cardElement);
   }
-
 }
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.png'>"; 
+	} else {
+		this.innerHTML = "<img src='queen.png'>"; 
+	}
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+createBoard();
